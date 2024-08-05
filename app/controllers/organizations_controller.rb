c6c6255c33108c519a7d821d3b05f6a785079c0a
Mavrick Laakso <laakso.mavrick@gmail.com>
2024-08-05 14:23:12 -0400

    In-code one org_membership per user

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class OrganizationsController < ApplicationController
-  before_action :authenticate_user!
-  def show
-    # TODO: authorization
-    @organization = current_user.organization # TODO: this should be whatever is selected at the moment
+  def index
+    @organization = current_user.organization
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe User do
   it 'creates a default org when none set' do
     user = create(:user)
     expect(user.organizations.count).not_to be(0)
-    expect(user.organizations.first.name).to eq(I18n.t('models.organization.fields.name.default'))
+    expect(user.organization.name).to eq(I18n.t('models.organization.fields.name.default'))
+    expect(user.organization_membership.role).to eq('admin')
+  end
+
+  it 'does not allow a user to have multiple organizations' do
+    user = create(:user)
+    user.organizations << create(:organization)
+    user.save
+    expect(user.errors.any?).to be(true)
   end
 end
